5f097a68047b754ef30c15d8800e6ba29311aecf
Juan_Taylor <juan13taylor@gmail.com>
2023-10-21 00:33:35 -0500

    subi carpeta lab7

---
+++ lab7/clases/miclase.php
@@ -0,0 +1,7 @@
+<?php
+class miclase{
+    public function__construct() {
+        echo "Mi Clase ha sido agregada!!!<br>";
+    }
+}
+?>
---
+++ lab7/clases/miotraclase.php
@@ -0,0 +1,7 @@
+<?php
+class miotraclase{
+    public function__construct() {
+        echo "Mi segunda Clase ha sido agregada!!!<br>";
+    }
+}
+?>
